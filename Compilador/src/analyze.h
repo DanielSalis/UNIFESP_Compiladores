b183be32ce698961ec465809f89b7ca6356f570e
DanielSalis <danielsalis98@gmail.com>
2022-02-07 22:43:28 -0300

    .

--- Compilador/src/analyze.h
+++ Compilador/src/analyze.h
@@ -1,10 +1,3 @@
-/****************************************************/
-/* File: analyze.h                                  */
-/* Semantic analyzer interface for TINY compiler    */
-/* Compiler Construction: Principles and Practice   */
-/* Kenneth C. Louden                                */
-/****************************************************/
-
 #ifndef _ANALYZE_H_
 #define _ANALYZE_H_
 
